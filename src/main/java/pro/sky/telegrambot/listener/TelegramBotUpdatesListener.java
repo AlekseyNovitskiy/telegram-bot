b551fbd21eabe80d724dcde8d01342b1bac5fba2
AlekseyNovitskiy <novickii_aleksei@mail.ru>
2023-01-17 16:30:33 +0300

    Задача 1. Сборка телеграм-бота

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,8 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,11 +26,23 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         telegramBot.setUpdatesListener(this);
     }
 
+
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
             // Process your updates here
+            switch(update.message().text()){
+                case "/start":
+                    SendMessage message = new SendMessage("5280286983", "Привет меня зовут Gary");
+                    SendResponse response = this.telegramBot.execute(message);
+                    response.isOk();
+                    break;
+                default:
+                    SendMessage message1 = new SendMessage("5280286983", "Некорректная команда");
+                    SendResponse response2 = this.telegramBot.execute(message1);
+                    response2.isOk();
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
